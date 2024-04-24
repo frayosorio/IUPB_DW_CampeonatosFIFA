fcafd9d5cea34a1fc067bad696da8c3446e03101
Fray León Osorio Rivera <frayosorio@gmail.com>
2024-04-23 19:42:33 -0500

    Se agrega componente para CRUD de SELECCIONES

--- src/app/features/componentes/seleccion/seleccion.component.ts
+++ src/app/features/componentes/seleccion/seleccion.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { Seleccion } from '../../../core/entidades/Seleccion';
 
 @Component({
   selector: 'app-seleccion',
   standalone: true,
-  imports: [],
+  imports: [
+    ReferenciasMaterialModule,
+    FormsModule,
+    NgxDatatableModule
+  ],
   templateUrl: './seleccion.component.html',
   styleUrl: './seleccion.component.css'
 })
 export class SeleccionComponent {
+
+  public textoBusqueda: String = "";
+  public selecciones: Seleccion[] = [];
+  public columnas=[
+    { name:"Selección", prop:"nombre"},
+    { name:"Entidad", prop:"entidad"}
+  ];
+
+  buscar() {
+
+  }
+  agregar() {
+
+  }
+  modificar() {
+
+  }
+  verificarEliminar() {
+
+  }
 
 }
